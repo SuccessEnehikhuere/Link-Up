23a96fc533199ecb484ee643107d2fdc948bd412
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-07 10:12:11 +0100

    "set up the dashboard page"

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useSearchParams } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import DashboardLayout from '@/components/Layouts/DashboardLayout'
+import Head from 'next/head'
+import Phonebg from '@/components/svgs/Dashboard/Phonebg'
+
+const Dashboard = () => {
+  const params = useSearchParams()
+  const page = params.get('page')
+  const { data: session } = useSession()
+
+  console.log(session)
+  return (
+    <DashboardLayout>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <main className="grid grid-cols-7 gap-6 mt-4 overflow-auto">
+        <section className="rounded-md col-span-3 hidden  lg:flex justify-center py-1 px-8 items-center bg-primary-white">
+          <article className="border relative border-dark-border-color rounded-[40px] p-2 py-4 flex justify-center items-center">
+            <section>
+              <Phonebg/>
+            </section>
+          </article>
+        </section>
+        {page === 'links' && <LinksPage />}
+        {page === 'profile' && <ProfilePage />}
+      </main>
+    </DashboardLayout>
+  )
+}
+
+export default Dashboard

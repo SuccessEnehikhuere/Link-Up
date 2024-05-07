c94b8dc9c1416e27005f1394693c6619d0d913e9
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-07 10:11:45 +0100

    "set up home page"

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,32 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="shortcut icon"
+            href="/icon.jpg"
+            style={{ borderRadius: '10px' }}
+          />
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="data:image/svg+xml,%3Csvg "
+          />
+          <link rel="apple-touch-icon" href="/icon.jpg" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="robots" content="follow, index" />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body className="bg-gray-100 text-gray-900 font-inter">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+export default MyDocument
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Head from "next/head"
+import AuthLayout from "@/components/Layouts/AuthLayout"
+import CircleSpinner from "@/components/Loaders/CircleSpinner"
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
-const inter = Inter({ subsets: ["latin"] });
+function Home() {
+  const router = useRouter()
+  const { status } = useSession()
 
-export default function Home() {
+  if (status === 'loading') {
+    return (
+      <section className="h-screen bg-primary-bg-light w-full flex justify-center items-center">
+        <CircleSpinner />
+      </section>
+    )
+  }
+
+  if (status === 'authenticated') {
+    router.push('/dashboard?page=links')
+  }
+
+  if (status === 'unauthenticated') {
+    router.push('/login')
+  }
+
   return (
-    <main>hi</main>
+    <AuthLayout>
+      <Head>
+        <title>DevLinks</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </AuthLayout>
   )
 }
+
+export default Home

63469eac0c566ad7a2056f6977bd8bcfe992e7bc
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-07 10:25:47 +0100

    "set up default styles"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary-white': '#fff',
+        'primary-color-light': '#737373',
+        'dark-grey': '#333',
+        'primary-btn-color': '#633CFF',
+        'primary-bg': '#FAFAFA',
+        'profile-bg': '#eeeeee',
+        'dark-border-color': '#737373',
       },
     },
   },
   plugins: [],
-};
+}

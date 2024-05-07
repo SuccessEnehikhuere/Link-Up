d214ae937f0d3a55694cefe83455abf122d67110
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-07 10:26:40 +0100

    "set up a circle spinner component, to indicate when a page is loading"

---
+++ components/Loaders/CircleSpinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Oval } from 'react-loader-spinner'
+
+const CircleSpinner = () => {
+ 	return (
+  <Oval visible={true} height='25' width='25' color='#ffffff' ariaLabel='oval-loading' wrapperStyle={{}} wrapperClass='' />
+)
+};
+
+
+export default CircleSpinner

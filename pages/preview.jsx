7c012e2d3b79414dc7e245a435030b95f071c334
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-07 10:24:37 +0100

    set up profile preview component"

---
+++ pages/preview.jsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import React from 'react'
+import PreviewNavbar from '@/components/Navbar/PreviewNavbar'
+import UserProfile from '@/components/Dashboard/Profile/UserProfile'
+
+const Preview = () => {
+  return (
+    <>
+      <Head>
+        <title>Preview</title>
+      </Head>
+      <section className="relative md:px-4 md:py-4 h-[35vh] md:bg-primary-btn-color max-w-screen w-full md:rounded-b-[2rem] box-border">
+        <div>
+          <PreviewNavbar />
+        </div>
+
+        <section className="w-full flex md:absolute md:top-1/2  justify-center ">
+          <section className="w-[300px] mt-10 md:mt-4 rounded-lg bg-primary-white ">
+            <UserProfile border="none" display="flex" Display="contents" />
+          </section>
+        </section>
+      </section>
+    </>
+  )
+}
+
+export default Preview

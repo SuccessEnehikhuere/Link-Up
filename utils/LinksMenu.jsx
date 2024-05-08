34a05e466e3ba38bf01b991fecefa441a937f45a
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-08 10:27:43 +0100

    "set up options for links  Select input"

---
+++ utils/LinksMenu.jsx
@@ -0,0 +1,150 @@
+import {
+  CodewarsIcon,
+  DevIcon,
+  FacebookIcon,
+  FreeCodeIcon,
+  FrontendMentorIcon,
+  GitIcon,
+  GitLabIcon,
+  HashNodeIcon,
+  LinkedinIcon,
+  StackIcon,
+  TwitchIcon,
+  TwitterIcon,
+  YoutubeIcon,
+} from "@/components/svgs/LinksMenu/SelectInputSvgs"
+
+
+export const LinksMenu = [
+  {
+    id: 1,
+    color: '#1A1A1A',
+    label: (
+      <span className="flex">
+        <GitIcon />
+        GitHub
+      </span>
+    ),
+  },
+  {
+    id: 2,
+    color: '#FFFFF',
+    label: (
+      <span className="flex">
+        <FrontendMentorIcon />
+        Frontend Mentor
+      </span>
+    ),
+  },
+  {
+    id: 3,
+    color: '#43B7E9',
+    label: (
+      <span className="flex">
+        <TwitterIcon />
+        Twitter
+      </span>
+    ),
+  },
+  {
+    id: 4,
+    color: '#2D68FF',
+    label: (
+      <span className="flex">
+        <LinkedinIcon />
+        LinkedIn
+      </span>
+    ),
+  },
+  {
+    id: 5,
+    color: '#EE3939',
+    label: (
+      <span className="flex">
+        <YoutubeIcon />
+        Youtube
+      </span>
+    ),
+  },
+  {
+    id: 6,
+    color: '#2442ac',
+    label: (
+      <span className="flex">
+        <FacebookIcon />
+        Facebook
+      </span>
+    ),
+  },
+
+  {
+    id: 7,
+    color: '#EE3FC8',
+    label: (
+      <span className="flex">
+        <TwitchIcon />
+        Twitch
+      </span>
+    ),
+  },
+  {
+    id: 8,
+    color: '#737373',
+    label: (
+      <span className="flex">
+        <DevIcon />
+        Dev.to
+      </span>
+    ),
+  },
+  {
+    id: 9,
+    color: '#8A1A50',
+    label: (
+      <span className="flex">
+        <CodewarsIcon />
+        Codewars
+      </span>
+    ),
+  },
+  {
+    id: 10,
+    color: '#302267',
+    label: (
+      <span className="flex">
+        <FreeCodeIcon />
+        freeCodeCamp
+      </span>
+    ),
+  },
+  {
+    id: 11,
+    color: '#EB4925',
+    label: (
+      <span className="flex">
+        <GitLabIcon />
+        Gitlab
+      </span>
+    ),
+  },
+  {
+    id: 12,
+    color: '#0330D1',
+    label: (
+      <span className="flex">
+        <HashNodeIcon />
+        HashNode
+      </span>
+    ),
+  },
+  {
+    id: 13,
+    color: '#EC7100',
+    label: (
+      <span className="flex">
+        <StackIcon />
+        Stack Overflow
+      </span>
+    ),
+  },
+]

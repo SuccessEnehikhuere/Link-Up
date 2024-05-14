cfff76d60490bf51750916638b1b235155ee607b
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-14 22:41:37 +0100

    "updates"

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import '@/styles/globals.css'
-
+import { Provider } from 'react-redux'
 import { SessionProvider } from 'next-auth/react'
-
+import store from '@/Redux/store/store'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Provider>
   )
 }

e9c809d0da8ad391b36ec5b61efb0a941250b774
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-16 15:57:23 +0100

    "set up user"s profile's image, name, and email, using useSelector to access the user"

--- components/Dashboard/Profile/UserProfile.jsx
+++ components/Dashboard/Profile/UserProfile.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import ProfileContainer from '@/components/ProfileBackground/ProfileContainer'
+import { useSelector } from 'react-redux'
 
 const UserProfile = () => {
+  const [loadingImage, setLoadingImage] = useState(true);
+  const {user, userLoading} = useSelector((state)=>state.user);
+  
+  //construct user's fullname 
+  const fullName = `${user?.firstName ? user?.firstName : ''} ${user?.lastName ? user?.lastName : ''}`
   return (
     <div className="flex md:shadow-lg px-6 box-border py-4 rounded-xl flex-col justify-between  gap-10 w-full">
       <section className="space-y-2 flex w-full px-4  flex-col items-center">
-        <div className="block relative rounded-full h-[100px] w-[100px] bg-profile-bg ">
-          <Image
-            // src={user?.image}
-            alt="profile-image"
-            layout="fill"
-            className='rounded-full bg-profile-bg'
-           
-          />
+        <div
+          className={`block relative rounded-full h-[100px] w-[100px] ${
+            !user?.image && 'bg-profile-bg '
+          } ${userLoading && 'shimmer'}`}
+        >
+          {user?.image && (
+            <Image
+              src={user?.image}
+              alt="profile-image"
+              layout="fill"
+              className={`rounded-full ${loadingImage ? 'shimmer' : ''}`}
+              onLoadingComplete={() => setLoadingImage(false)}
+            />
+          )}
         </div>
-        <p className="flex-nowrap flex flex-col justify-center text-dark-grey-color-light text-lg items-center rounded-lg h-4 w-full font-bold truncate px-1 bg-profile-bg"></p>
-        <p className="flex flex-col text-primary-color-light text-sm  justify-center items-center rounded-lg h-2 w-20 bg-profile-bg"></p>
+        <p
+          className={`flex-nowrap flex flex-col justify-center text-dark-grey text-lg items-center rounded-lg h-4 w-full font-bold truncate px-1 ${
+            !user?.firstName && 'bg-profile-bg'
+          }  ${userLoading && 'shimmer'}`}
+        >
+          {' '}
+          {fullName}
+        </p>
+        <p
+          className={`flex flex-col text-primary-color-light text-sm  justify-center items-center rounded-lg h-2 w-20 ${
+            !user?.email && 'bg-profile-bg'
+          }${userLoading && 'shimmer'} `}
+        >
+          {' '}
+          {user?.email && user?.email}
+        </p>
       </section>
-       <ProfileContainer/>
+      <ProfileContainer />
     </div>
   )
 }

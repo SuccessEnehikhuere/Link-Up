b66686c04a27d8cbabfa8d81a3d8f5d94dcdc1d3
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-17 11:35:47 +0100

    "set up the component, for rendering the copy link alert"

---
+++ components/AlertModals/CopyLinkAlert.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleShowLink } from '@/Redux/slices/copyLinkAlertSlice';
+
+const CopyLinkAlert = () => {
+  const [timer, setTimer] = useState(2);
+  const dispatch = useDispatch();
+  const { showLinkAlert } = useSelector((state) => state.alert)
+
+ useEffect(() => {
+   setTimeout(() => {
+     if (timer === 0) {
+       setTimer(2)
+       dispatch(toggleShowLink(false));
+       return;
+     }
+     if (showLinkAlert) {
+       setTimer((prev) => prev - 1)
+     }
+   }, 2000)
+ }, [timer, showLinkAlert])
+
+  return (
+    <section
+      className={`fixed bottom-8 z-[10000] left-0 right-0 w-full flex justify-center ${
+        showLinkAlert ? 'block showModal ' : 'hidden'
+      }`}
+    >
+      <section className=" flex items-center px-4 py-2 bg-dark-grey-color-light w-auto text-light-Grey rounded-lg ">
+        <LinkIcon />
+        <p className="text-sm ml-2">
+          The link has been copied to your clipboard!
+        </p>
+      </section>
+    </section>
+  )
+}
+
+export default CopyLinkAlert

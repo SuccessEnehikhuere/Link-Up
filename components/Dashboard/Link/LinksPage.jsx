8a4862b27caec7fbcc6206dd9a8e1648d1736a5a
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-17 11:30:52 +0100

    "set up the functionality for adding newLinks"

--- components/Dashboard/Link/LinksPage.jsx
+++ components/Dashboard/Link/LinksPage.jsx
@@ -1,21 +1,17 @@
 import React, { useState } from 'react'
 import GetStarted from './GetStarted';
 import AddLinks from './AddLinks';
-// import AddLinks from './AddLinks'
-// import GetStarted from './GetStarted'
-// import useDashboardHook from '../hooks/useDashboardHook'
-// import SaveSpinner from '../../LoadingSpinners/SaveSpinner'
+import useDashboardHook from '../hooks/useDashboardHook';
+import SaveSpinner from '@/components/Loaders/saveSpinner';
 
 const LinksPage = () => {
-  
-  // const {
-  //   saveLinksLoading,
-  //   handleAddNewLink,
-  //   activateSaveBtn,
-  //   newLinks,
-  //   handleSaveLinks,
-  // } = useDashboardHook()
-
+ const {
+   saveLinksLoading,
+   handleAddNewLink,
+   activateSaveBtn,
+   newLinks,
+   handleSaveLinks,
+ } = useDashboardHook()
 
   return (
     <section className="col-span-7 lg:col-span-4 bg-primary-white rounded-md p-6 md:px-12  overflow-auto">
@@ -27,24 +23,27 @@ const LinksPage = () => {
         world!
       </p>
       <button
-        // disabled={newLinks.length >= 2}
-        className="border rounded-md py-1 
-            bg-primary-white border-primary-btn-color font-[600] text-primary-btn-color
-         flex items-center justify-center w-full"
+        disabled={newLinks.length >= 2}
+        className={`border rounded-md py-1 ${
+          newLinks.length >= 2
+            ? 'text-primary-color-light bg-gray-100'
+            : 'bg-primary-white border-primary-btn-color font-[600] text-primary-btn-color'
+        } flex items-center justify-center w-full`}
+        onClick={handleAddNewLink}
       >
         + Add new link
       </button>
       <section className="mt-4 divide-y space-y-4">
-        <AddLinks />
-        {/* <GetStarted /> */}
-        {/* {newLinks < 0 ? <AddLinks /> : <GetStarted />} */}
+        {newLinks.length > 0 ? <AddLinks /> : <GetStarted />}
         <section className="flex w-full justify-end pt-4">
           <button
-            // onClick={handleSaveLinks}
-            className="bg-primary-btn-color w-full flex justify-center items-center md:w-[100px] text-white px-6 py-2 rounded-md"
+            onClick={handleSaveLinks}
+            className={`bg-primary-btn-color w-full flex justify-center items-center md:w-[100px] text-white px-6 py-2 rounded-md ${
+              activateSaveBtn ? 'opacity-100' : 'opacity-25'
+            }`}
           >
             save
-            {/* {saveLinksLoading ? <SaveSpinner /> : 'Save'} */}
+            {saveLinksLoading ? <SaveSpinner /> : 'Save'}
           </button>
         </section>
       </section>

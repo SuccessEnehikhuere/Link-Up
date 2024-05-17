c2b87e371a581ba1a2d8dfe8581895cc08dbfd92
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-17 11:33:29 +0100

    "set up custom hook, for handling the addition of links, updating and deleting links"

---
+++ components/Dashboard/hooks/useDashboardHook.jsx
@@ -0,0 +1,115 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import { fetchLinksFromDb, increaseLinks, removeLink, saveLinksToDb, toggleActivateSaveBtn, updateLink } from "@/Redux/slices/helperSlice";
+import { useSession } from "next-auth/react";
+
+const useDashboardHook = ()=>{
+  const {data: session} = useSession();
+  const dispatch = useDispatch();
+  const [activateSave, setActivateSave] = useState(false);
+  const {noOfLinks, activateSaveBtn, newLinks, saveLinksLoading } = useSelector((state)=>state.helper);
+
+  const styles = {
+    dropdownIndicator: (base) => ({
+      padding: 0,
+      margin: '0 0.5rem',
+    }),
+    indicatorSeparator: (base) => ({
+      display: 'none',
+    }),
+    option: (base, state) => ({
+      ...base,
+      background: state.isSelected
+        ? '#f5f5f5'
+        : state.isDisabled
+        ? '#EFEBFF'
+        : 'transparent',
+      color: state.isSelected ? '#333' : state.isDisabled ? '#737373' : '#333',
+      padding: '0.5rem 1rem',
+      fontSize: '14px',
+      fontWeight: '400',
+      borderRadius: '0.5rem',
+      cursor: state.isSelected
+        ? 'pointer'
+        : state.isDisabled
+        ? 'not-allowed'
+        : 'pointer',
+      '&:hover': {
+        background: state.isSelected
+          ? '#f5f5f5'
+          : state.isDisabled
+          ? '#EFEBFF'
+          : '#f5f5f5',
+      },
+    }),
+    control: (base) => ({
+      ...base,
+      border: '1px solid #d9d9d9',
+      background: 'transparent',
+      width: '100%',
+      boxShadow: 'none',
+      '&:focus': {
+        outline: 'none',
+      },
+      '&:hover': {
+        outline: 'none',
+        border: '1px solid #d9d9d9',
+      },
+    }),
+  };
+
+  const handleAddNewLink = ()=>{
+    dispatch(increaseLinks(session.user.id));
+  };
+
+  const handleRemoveLink = (id)=>{
+    dispatch(removeLink(id));
+  };
+
+  const updateLinks = (id, selectedOption, linkAddress)=>{
+    dispatch(
+      updateLink({
+        id,
+        address: linkAddress,
+        name: selectedOption.value,
+        color: selectedOption.color,
+      })
+    )
+  };
+
+  const handleSaveLinks = () => {
+    dispatch(saveLinksToDb(newLinks))
+  };
+
+ const handleActivateSave = (selectedOption, linkAddress) => {
+   dispatch(toggleActivateSaveBtn(true))
+ };
+
+  const deActivateSave = () => {
+    dispatch(toggleActivateSaveBtn(false))
+  };
+
+  const fetchLinks = () => {
+    dispatch(fetchLinksFromDb())
+  };
+
+  return {
+    styles,
+    noOfLinks,
+    handleAddNewLink,
+    handleRemoveLink,
+    activateSave,
+    setActivateSave,
+    handleActivateSave,
+    deActivateSave,
+    activateSaveBtn,
+    newLinks,
+    updateLinks,
+    handleSaveLinks,
+    saveLinksLoading,
+    fetchLinks,
+  }
+
+};
+
+export default useDashboardHook

04c7ebb791ac714edeb8a0ba5fe24bf1f28af112
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-18 20:02:01 +0100

    "remove redundant code"

--- pages/preview.jsx
+++ pages/preview.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import React from 'react'
 import PreviewNavbar from '@/components/Navbar/PreviewNavbar'
-import UserProfile from '@/components/Dashboard/Profile/UserProfile'
+import UserProfile from '@/components/Preview/UserProfile'
 
 const Preview = () => {
   return (
@@ -16,7 +16,7 @@ const Preview = () => {
 
         <section className="w-full flex md:absolute md:top-1/2  justify-center ">
           <section className="w-[300px] mt-10 md:mt-4 rounded-lg bg-primary-white ">
-            <UserProfile border="none" display="flex" Display="contents" />
+            <UserProfile />
           </section>
         </section>
       </section>

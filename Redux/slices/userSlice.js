3a3078d2841fc7cf68419a15477428c94e9fab21
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-30 14:39:10 +0100

    "chore : update the function, for saving links to the database"

--- Redux/slices/userSlice.js
+++ Redux/slices/userSlice.js
@@ -10,16 +10,16 @@ const initialState = {
   showChangesAlert: false,
 }
 
-export const updateUser = createAsyncThunk('user/updateUser', async(user)=>{
-  const response = await fetch('api/auth/updateUser', {
-    method:'PUT',
-    headers:{
+export const updateUser = createAsyncThunk('user/updateUser', async (user) => {
+  const response = await fetch('/api/auth/updateUser', {
+    method: 'PUT',
+    headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(user),
-  });
-  const data = await response.json();
-  return data;
+  })
+  const data = await response.json()
+  return data
 })
 
 export const fetchUser = createAsyncThunk('user/fetchuser', async()=>{
@@ -34,45 +34,43 @@ export const fetchUser = createAsyncThunk('user/fetchuser', async()=>{
 })
 
 export const userSlice = createSlice({
-  name:'user',
+  name: 'user',
   initialState,
-  reducers:{
-    toggleChangesModal: (state, action)=>{
+  reducers: {
+    toggleChangesModal: (state, action) => {
       state.showChangesAlert = action.payload
-    }
+    },
   },
-  extraReducers:(builder)=>{
-    builder.addCase(updateUser.pending, (state)=>{
-      state.updateUserLoading = true;
-      state.showChangesAlert = false;
-    });
-    builder.addCase(updateUser.fulfilled, (state, action)=>{
-			console.log(action.payload)
-      state.updateUserLoading = false;
+  extraReducers: (builder) => {
+    builder.addCase(updateUser.pending, (state) => {
+      state.updateUserLoading = true
+      state.showChangesAlert = false
+    })
+    builder.addCase(updateUser.fulfilled, (state, action) => {
+      console.log(action.payload)
+      state.updateUserLoading = false
       state.showChangesAlert = true
       state.user = action.payload.data
       state.updateUserMessage = 'Your changes have been successfully saved!'
-    });
-    builder.addCase(updateUser.rejected, (state)=>{
-       state.updateUserLoading = false;
-       state.updateUserMessage =
-         'There was an error saving your changes, please try again later!!!';
-       state.showChangesAlert = false
-    });
-    builder.addCase(fetchUser.pending, (state)=>{
-      state.userLoading = true;
-
-    });
+    })
+    builder.addCase(updateUser.rejected, (state) => {
+      state.updateUserLoading = false
+      state.updateUserMessage =
+        'There was an error saving your changes, please try again later!!!'
+      state.showChangesAlert = false
+    })
+    builder.addCase(fetchUser.pending, (state) => {
+      state.userLoading = true
+    })
     builder.addCase(fetchUser.fulfilled, (state, action) => {
-      state.userLoading = false;
-      state.user = action.payload?.data;
-
-    });
+      state.userLoading = false
+      state.user = action.payload?.data
+    })
     builder.addCase(fetchUser.rejected, (state, action) => {
-        state.userLoading = false;
-        state.userError = action.payload
-    });
-  }
-});
+      state.userLoading = false
+      state.userError = action.payload
+    })
+  },
+})
 export default userSlice.reducer;
 export const {toggleChangesModal} = userSlice.actions;

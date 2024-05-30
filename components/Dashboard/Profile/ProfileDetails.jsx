03eb0b39323898caee4d2af43033d741fef8012b
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-05-30 14:12:11 +0100

    "chore: update input validation"

--- components/Dashboard/Profile/ProfileDetails.jsx
+++ components/Dashboard/Profile/ProfileDetails.jsx
@@ -8,11 +8,11 @@ const ProfileDetails = ({ register, updateUserLoading, email }) => {
       <div className=" items-center grid md:grid-cols-12  justify-between text-primary-color-light   gap-1  md:gap-8">
         <p className="w-auto col-span-4">First Name*</p>
         <input
-          className="flex-1 col-span-8 border px-3 py-2 bg-primary-white-light rounded-md placeholder:text-xs focus:outline-none text-dark-grey-color-light focus:border-purple focus:ring-0 focus:shadow-purple focus:ring-purple"
+          className="flex-1 col-span-8 border px-3 py-2 bg-primary-white rounded-md placeholder:text-xs focus:outline-none text-dark-grey focus:border-purple focus:ring-0 focus:shadow-purple focus:ring-purple"
           type="text"
           placeholder="Enter your name"
-          // {...register('firstName')}
-          // disabled={updateUserLoading}
+          {...register('firstName')}
+          disabled={updateUserLoading}
         />
       </div>
       <div className="grid md:grid-cols-12 items-center  text-primary-color-light   gap-1  md:gap-8">
@@ -20,19 +20,19 @@ const ProfileDetails = ({ register, updateUserLoading, email }) => {
         <input
           className="flex-1  border col-span-8 px-3 py-2 bg-primary-white rounded-md placeholder:text-xs focus:outline-none text-dark-grey focus:border-purple focus:ring-0 focus:shadow-purple focus:ring-purple"
           type="text"
-          placeholder="Enter your bio"
-          // {...register('lastName')}
-          // disabled={updateUserLoading}
+          placeholder="Enter your Last Name"
+          {...register('lastName')}
+          disabled={updateUserLoading}
         />
       </div>
       <div className="items-center grid md:grid-cols-12  text-primary-color-light gap-1  md:gap-8">
-        <p className="w-auto col-span-4">Email</p>
+        <p className="w-auto col-span-4">Email*</p>
         <input
           className="flex-1 col-span-8 border px-3 py-2 bg-primary-white rounded-md placeholder:text-xs focus:outline-none text-dark-grey focus:border-purple focus:ring-0 focus:shadow-purple focus:ring-purple"
           type="text"
           placeholder="Enter your email"
-          disabled
-          defaultValue={email}
+          disabled={updateUserLoading}
+          // defaultValue={email}
         />
       </div>
     </section>

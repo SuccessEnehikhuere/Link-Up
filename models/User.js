f9d447297db8e90aeb6e2b32655684e29fa2906e
Success Enehikhuere <enehikhueresuccess@gmail.com>
2025-07-25 12:38:45 +0100

    "updates"

--- models/User.js
+++ models/User.js
@@ -1,24 +1,4 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     firstName: { type: String, required: false },
-//     lastName: { type: String, required: false },
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String },
-//     image: { type: String },
-//   },
-
-//   {
-//     timestamps: true,
-//   }
-// )
-
-// const User = mongoose.models.User || mongoose.model('User', userSchema)
-// export default User
-
-import mongoose from 'mongoose'
-import bcryptjs from 'bcryptjs'
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -28,25 +8,11 @@ const userSchema = new mongoose.Schema(
     password: { type: String },
     image: { type: String },
   },
+
   {
     timestamps: true,
   }
 )
-
-// Adding a method to compare hashed password with plain text password
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return bcryptjs.compare(candidatePassword, this.password)
-}
-
-// Ensure to hash the password before saving the user
-userSchema.pre('save', async function (next) {
-  if (this.isModified('password') || this.isNew) {
-    // Hash the password
-    const salt = await bcryptjs.genSalt(10)
-    this.password = await bcryptjs.hash(this.password, salt)
-  }
-  next()
-})
 
 const User = mongoose.models.User || mongoose.model('User', userSchema)
 export default User
